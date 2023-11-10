dff76ebec1f14fd68820f96511c3a4cf5ed06377
Krishan Sahdev <krishansahdev00@gmail.com>
2023-11-10 14:56:29 -0500

    no move bug fixed???

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    public float timer = 0.5f;
+    public float timer = 2f;
     
     // Start is called before the first frame update
     void Start()
